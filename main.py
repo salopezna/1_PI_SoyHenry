b99975e44b972b73f8a7f8f1f3a8d1ab1e3b547e
SergioALopezN <56182394+salopezna@users.noreply.github.com>
2025-02-19 07:35:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("src.api:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
 import pandas as pd 
 import numpy as np 
